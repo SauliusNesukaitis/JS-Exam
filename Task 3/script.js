e8cbdc6966e6bd1790385217ae1986e4ab1ebf27
Saulius <sauliusnesukaitis@gmail.com>
2022-05-11 18:19:44 +0300

    Task3 done

--- Task 3/script.js
+++ Task 3/script.js
@@ -12,3 +12,41 @@ turėti bent minimalų stilių ir būti responsive;
 -------------------------------------------------------------------------- */
 
 const ENDPOINT = 'https://api.github.com/users';
+const showUsersBtn = document.getElementById('btn');
+const mesageElement = document.getElementById('message');
+const outputElement = document.getElementById('output');
+
+function getUser() {
+    showUsersBtn.addEventListener('click', () => {
+    mesageElement.remove();
+    fetch(ENDPOINT)
+        .then(response => response.json())
+        .then(data => {
+            data.forEach(element => {
+                const card = document.createElement('div');
+                card.classList.add("card");
+
+                const userImageOutput = createElement('img',element.avatar_url);
+                userImageOutput.classList.add('image');
+                card.appendChild(userImageOutput);
+
+                const userLoginOutput = createElement('h3',element.login);       
+                card.appendChild(userLoginOutput);
+
+                outputElement.appendChild(card);
+            });
+        })
+    })
+};
+
+getUser();
+
+function createElement(tag, text) {
+    const element = document.createElement(tag);
+    if(tag === 'img') {
+        element.src = text;
+    } else {
+        element.innerHTML = text; 
+    }
+    return element;
+}
