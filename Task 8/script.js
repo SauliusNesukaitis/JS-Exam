11ec68036b2fbcdd6a9976b5c0ea9573819d6035
Saulius <sauliusnesukaitis@gmail.com>
2022-05-14 11:08:31 +0300

    Task8 done

--- Task 8/script.js
+++ Task 8/script.js
@@ -5,3 +5,27 @@ subtraction(a, b) - priima du skaičius ir grąžina jų skirtumą.
 multiplication(a, b) - priima du skaičius ir grąžina jų daugybos rezultatą;
 division(a, b) - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+
+function Calculator(a, b) {
+    this.a = a;
+    this.b = b;
+    this.sum = function sum(a, b) {
+        console.log(a + b);
+    };
+    this.sub = function sub(a, b) {
+        console.log(a - b);
+    }
+    this.mul = function mul(a, b) {
+        console.log(a * b);
+    }
+    this.div = function div(a, b) {
+        console.log(Math.round(a / b * 100) / 100);
+    }
+};
+  
+let calculator = new Calculator();
+
+calculator.sum(4, 9)
+calculator.sub(4, 9)
+calculator.mul(4, 9)
+calculator.div(4, 9)
